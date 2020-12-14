7a9550b664ae5fdfe9964ba75f53595d725f4843
Aiden Gu <aiden.gu01@gmail.com>
2020-12-14 21:12:33 +1100

    Construct layout for clock timer, run into exception thread error

--- app/src/main/java/com/example/clockofeidolon/MainActivity.kt
+++ app/src/main/java/com/example/clockofeidolon/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.clockofeidolon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.clockofeidolon.clock.Clock
+import com.example.clockofeidolon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.clock = Clock()
     }
-
-
 }
